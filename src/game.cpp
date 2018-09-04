0da59b2e89652b6a4fceddd1d56c9856d014813a
Oliver Faircliff <mail@oliverfaircliff.com>
2018-09-04 20:59:01 +0100

    Refactor game loop into Game::State struct

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,41 @@
 #include "game.h"
 
+#include "sdl-state.h"
+#include "texture.h"
+#include "input.h"
+#include "assert.h"
+
+
 namespace Game
 {
+
+bool
+advance(State *state, SDL_State::SDL_State& sdl_state)
+{
+  bool success = true;
+
+  FrameID::update(state->frame_id);
+  Input::update(state->input, state->frame_id);
+
+  if (state->input.quit)
+  {
+    success = false;
+  }
+
+  if (Input::up<Key::F11>(state->input, state->frame_id))
+  {
+    SDL_State::toggle_fullscreen(sdl_state);
+  }
+
+  static int n = 0;
+  n += 67;
+  Texture::set_pixel(state->texture, (n/state->texture.height)%state->texture.width, n%state->texture.height, (0xffd0043+n*437347%57342)*9);
+
+  assert(SDL_State::render(sdl_state, state->texture));
+
+  return success;
+}
+
 
 bool
 run()
@@ -13,7 +47,29 @@ run()
 
   if (success)
   {
-    success &= GameLoop::game_loop(sdl_state);
+    State state = {};
+
+    state.width = 128;
+    state.height = 128;
+
+    success &= Texture::init(state.texture, state.width, state.height);
+
+    if (success)
+    {
+      SDL_State::set_render_texture(sdl_state, state.texture);
+
+      Input::init(state.input);
+
+      state.frame_id = 0;
+
+      bool running = true;
+      while (running)
+      {
+        running &= advance(&state, sdl_state);
+      }
+
+      Texture::destroy(state.texture);
+    }
   }
 
   SDL_State::destroy(sdl_state);
--- src/game.h
+++ src/game.h
@@ -1,12 +1,25 @@
 #pragma once
 
-#include "sdl-state.h"
-#include "game-loop.h"
+#include "texture.h"
+#include "input.h"
+#include "frame-id.h"
 #include "types.h"
 
 
 namespace Game
 {
+
+struct State
+{
+  s32 width;
+  s32 height;
+
+  Texture::Texture texture;
+
+  FrameID::FrameID frame_id;
+  Input::Input input;
+};
+
 
 bool
 run();
