80a3c100d7493e9daacc13b6480b81b5df081bb8
Oliver Faircliff <mail@oliverfaircliff.com>
2019-07-05 22:38:44 +0100

    FunctionInfo's for built-in functions

--- src/built-in-code.h
+++ src/built-in-code.h
@@ -23,133 +23,182 @@ using Machine::MemoryAddress;
 
 
 // TODO: These should probably be inlined
+// TODO: Write an inliner at the ast stage
 
 
 inline
-MemoryAddress
+FunctionInfo
 generate_add8_code(CodeGenerator& code_generator, MemoryAddress& addr)
 {
-  MemoryAddress return_addr = Machine::advance_addr<MemoryAddress>(addr);
-  MemoryAddress return_value = Machine::advance_addr<u8>(addr);
+  FunctionInfo result = {};
+  result.return_value_bytes += 1;
+  result.data_start = addr;
+
+  result.return_address = Machine::advance_addr<MemoryAddress>(addr);
+  result.return_value = Machine::advance_addr<u8>(addr);
 
   // Params
   MemoryAddress param_a = Machine::advance_addr<u8>(addr);
+  result.parameter_addrs += param_a;
+
   MemoryAddress param_b = Machine::advance_addr<u8>(addr);
+  result.parameter_addrs += param_b;
 
   // Code
-  MemoryAddress result = addr;
+  result.data_end = addr;
+  result.code_start = result.data_end;
 
   Basolls::push_instruction<Code::ADD>(*code_generator.machine, addr, {
     .a = param_a,
     .b = param_b,
-    .result = return_value
+    .result = result.return_value
   });
 
   Basolls::push_instruction<Code::JUMP>(*code_generator.machine, addr, {
-    .addr = return_addr
+    .addr = result.return_address
   });
+
+  result.code_end = addr;
 
   return result;
 }
 
 
 inline
-MemoryAddress
+FunctionInfo
 generate_add16_code(CodeGenerator& code_generator, MemoryAddress& addr)
 {
-  MemoryAddress return_addr = Machine::advance_addr<MemoryAddress>(addr);
-  MemoryAddress return_value = Machine::advance_addr<u16>(addr);
+  FunctionInfo result = {};
+  result.return_value_bytes += 2;
+  result.data_start = addr;
+
+  result.return_address = Machine::advance_addr<MemoryAddress>(addr);
+  result.return_value = Machine::advance_addr<u16>(addr);
 
   // Params
   MemoryAddress param_a = Machine::advance_addr<u16>(addr);
+  result.parameter_addrs += param_a;
+
   MemoryAddress param_b = Machine::advance_addr<u16>(addr);
+  result.parameter_addrs += param_b;
 
   // Code
-  MemoryAddress result = addr;
+  result.data_end = addr;
+  result.code_start = result.data_end;
 
   Basolls::push_instruction<Code::ADD_W>(*code_generator.machine, addr, {
     .a = param_a,
     .b = param_b,
-    .result = return_value
+    .result = result.return_value
   });
 
   Basolls::push_instruction<Code::JUMP>(*code_generator.machine, addr, {
-    .addr = return_addr
+    .addr = result.return_address
   });
+
+  result.code_end = addr;
 
   return result;
 }
 
 
 inline
-MemoryAddress
+FunctionInfo
 generate_sub8_code(CodeGenerator& code_generator, MemoryAddress& addr)
 {
-  MemoryAddress return_addr = Machine::advance_addr<MemoryAddress>(addr);
-  MemoryAddress return_value = Machine::advance_addr<u8>(addr);
+  FunctionInfo result = {};
+  result.return_value_bytes += 1;
+  result.data_start = addr;
+
+  result.return_address = Machine::advance_addr<MemoryAddress>(addr);
+  result.return_value = Machine::advance_addr<u8>(addr);
 
   // Params
   MemoryAddress param_a = Machine::advance_addr<u8>(addr);
+  result.parameter_addrs += param_a;
+
   MemoryAddress param_b = Machine::advance_addr<u8>(addr);
+  result.parameter_addrs += param_b;
 
   // Code
-  MemoryAddress result = addr;
+  result.data_end = addr;
+  result.code_start = result.data_end;
 
   Basolls::push_instruction<Code::SUB>(*code_generator.machine, addr, {
     .a = param_a,
     .b = param_b,
-    .result = return_value
+    .result = result.return_value
   });
 
   Basolls::push_instruction<Code::JUMP>(*code_generator.machine, addr, {
-    .addr = return_addr
+    .addr = result.return_address
   });
+
+  result.code_end = addr;
 
   return result;
 }
 
 
 inline
-MemoryAddress
+FunctionInfo
 generate_sub16_code(CodeGenerator& code_generator, MemoryAddress& addr)
 {
-  MemoryAddress return_addr = Machine::advance_addr<MemoryAddress>(addr);
-  MemoryAddress return_value = Machine::advance_addr<u16>(addr);
+  FunctionInfo result = {};
+  result.return_value_bytes += 2;
+  result.data_start = addr;
+
+  result.return_address = Machine::advance_addr<MemoryAddress>(addr);
+  result.return_value = Machine::advance_addr<u16>(addr);
 
   // Params
   MemoryAddress param_a = Machine::advance_addr<u16>(addr);
+  result.parameter_addrs += param_a;
+
   MemoryAddress param_b = Machine::advance_addr<u16>(addr);
+  result.parameter_addrs += param_b;
 
   // Code
-  MemoryAddress result = addr;
+  result.data_end = addr;
+  result.code_start = result.data_end;
 
   Basolls::push_instruction<Code::SUB_W>(*code_generator.machine, addr, {
     .a = param_a,
     .b = param_b,
-    .result = return_value
+    .result = result.return_value
   });
 
   Basolls::push_instruction<Code::JUMP>(*code_generator.machine, addr, {
-    .addr = return_addr
+    .addr = result.return_address
   });
+
+  result.code_end = addr;
 
   return result;
 }
 
 
 inline
-MemoryAddress
+FunctionInfo
 generate_put8_code(CodeGenerator& code_generator, MemoryAddress& addr)
 {
-  MemoryAddress return_addr = Machine::advance_addr<MemoryAddress>(addr);
-  MemoryAddress return_value = Machine::advance_addr<u8>(addr);
+  FunctionInfo result = {};
+  result.return_value_bytes += 2;
+  result.data_start = addr;
+
+  result.return_address = Machine::advance_addr<MemoryAddress>(addr);
+  result.return_value = Machine::advance_addr<u8>(addr);
 
   // Params
   MemoryAddress address = Machine::advance_addr<MemoryAddress>(addr);
+  result.parameter_addrs += address;
+
   MemoryAddress value = Machine::advance_addr<u8>(addr);
+  result.parameter_addrs += value;
 
   // Code
-  MemoryAddress result = addr;
+  result.data_end = addr;
+  result.code_start = result.data_end;
 
   Basolls::push_instruction<Code::SET>(*code_generator.machine, addr, {
     .from = value,
@@ -157,8 +206,10 @@ generate_put8_code(CodeGenerator& code_generator, MemoryAddress& addr)
   });
 
   Basolls::push_instruction<Code::JUMP>(*code_generator.machine, addr, {
-    .addr = return_addr
+    .addr = result.return_address
   });
+
+  result.code_end = addr;
 
   return result;
 }
@@ -168,41 +219,76 @@ inline
 void
 generate_code(CodeGenerator& code_generator, Identifiers::BuiltIn built_in_identifiers, FunctionScope& func)
 {
-  func.identifiers_map += {
-    built_in_identifiers.add8,
-    {
-      .address = generate_add8_code(code_generator, func.info.data_end),
-      .debug_name = Strings::add(*code_generator.strings, "add8")
-    }
-  };
-  func.identifiers_map += {
-    built_in_identifiers.add16,
-    {
-      .address = generate_add16_code(code_generator, func.info.data_end),
-      .debug_name = Strings::add(*code_generator.strings, "add16")
-    }
-  };
-  func.identifiers_map += {
-    built_in_identifiers.sub8,
-    {
-      .address = generate_sub8_code(code_generator, func.info.data_end),
-      .debug_name = Strings::add(*code_generator.strings, "sub8")
-    }
-  };
-  func.identifiers_map += {
-    built_in_identifiers.sub16,
-    {
-      .address = generate_sub16_code(code_generator, func.info.data_end),
-      .debug_name = Strings::add(*code_generator.strings, "sub16")
-    }
-  };
-  func.identifiers_map += {
-    built_in_identifiers.put8,
-    {
-      .address = generate_put8_code(code_generator, func.info.data_end),
-      .debug_name = Strings::add(*code_generator.strings, "put8")
-    }
-  };
+  {
+    FunctionInfo func_info = generate_add8_code(code_generator, func.info.data_end);
+    func.identifiers_map += {
+      built_in_identifiers.add8,
+      {
+        .address = func_info.code_start,
+        .debug_name = Strings::add(*code_generator.strings, "add8")
+      }
+    };
+    func.functions_map += {
+      built_in_identifiers.add8,
+      func_info
+    };
+  }
+  {
+    FunctionInfo func_info = generate_add16_code(code_generator, func.info.data_end);
+    func.identifiers_map += {
+      built_in_identifiers.add16,
+      {
+        .address = func_info.code_start,
+        .debug_name = Strings::add(*code_generator.strings, "add16")
+      }
+    };
+    func.functions_map += {
+      built_in_identifiers.add16,
+      func_info
+    };
+  }
+  {
+    FunctionInfo func_info = generate_sub8_code(code_generator, func.info.data_end);
+    func.identifiers_map += {
+      built_in_identifiers.sub8,
+      {
+        .address = func_info.code_start,
+        .debug_name = Strings::add(*code_generator.strings, "sub8")
+      }
+    };
+    func.functions_map += {
+      built_in_identifiers.sub8,
+      func_info
+    };
+  }
+  {
+    FunctionInfo func_info = generate_sub16_code(code_generator, func.info.data_end);
+    func.identifiers_map += {
+      built_in_identifiers.sub16,
+      {
+        .address = func_info.code_start,
+        .debug_name = Strings::add(*code_generator.strings, "sub16")
+      }
+    };
+    func.functions_map += {
+      built_in_identifiers.sub16,
+      func_info
+    };
+  }
+  {
+    FunctionInfo func_info = generate_put8_code(code_generator, func.info.data_end);
+    func.identifiers_map += {
+      built_in_identifiers.put8,
+      {
+        .address = func_info.code_start,
+        .debug_name = Strings::add(*code_generator.strings, "put8")
+      }
+    };
+    func.functions_map += {
+      built_in_identifiers.put8,
+      func_info
+    };
+  }
 }
 
 } // namespace BuiltInCode
