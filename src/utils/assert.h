0c40e16a5e43656ed85e1bb1945229a16d14d788
Oliver Faircliff <mail@oliverfaircliff.com>
2019-10-13 18:10:25 +0100

    Custom assert macro

--- src/utils/assert.h
+++ src/utils/assert.h
@@ -1,7 +1,21 @@
 #pragma once
 
-#include <assert.h>
+#include "utils/debug.h"
 
+#define trap() __asm__ __volatile__ ("int $3\n\t")
+
+#define S(x) #x
+#define S_(x) S(x)
+#define S__LINE__ S_(__LINE__)
+
+#define assert(cond) \
+  { \
+    if (!(cond)) \
+    { \
+      printf("assert(" #cond ") failed at " __FILE__ ":" S__LINE__ "\n"); \
+      trap(); \
+    } \
+  }
 
 namespace Assert
 {
