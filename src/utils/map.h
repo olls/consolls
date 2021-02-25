5789297a7699da977054f806ba7008236d446f84
Oliver Faircliff <faircliff.oliver@gmail.com>
2021-02-25 21:05:49 +0000

    Add ensure, erase, find_index to map

--- src/utils/map.h
+++ src/utils/map.h
@@ -2,23 +2,27 @@
 
 #include "utils/array.h"
 #include "utils/types.h"
+#include "utils/pair.h"
 
 
 namespace Map
 {
 
-template <typename Key_t, typename Value_t>
-struct Pair
-{
-  using Key = Key_t;
-  using Value = Value_t;
-
-  Key key;
-  Value value;
-};
-
 template <typename Key, typename Value>
 using Map = Array::Array<Pair<Key, Value>>;
+
+
+template <typename Key, typename Value>
+s32
+find_index(Map<Key, Value> const& map, Key key)
+{
+  using PairT = Pair<Key, Value>;
+
+  Array::FindFirstFunc<PairT> find_func = [](PairT const& pair, void* user){ return pair.key == *(Key*)user; };
+  s32 index = Array::find_first(map, find_func, &key);
+
+  return index;
+}
 
 
 template <typename Key, typename Value>
@@ -43,18 +47,54 @@ template <typename Key, typename Value>
 Value*
 find(Map<Key, Value>& map, Key key)
 {
-  using PairT = Pair<Key, Value>;
-
   Value* result = NULL;
 
-  Array::FindFirstFunc<PairT> find_func = [](PairT const& pair, void* user){ return pair.key == *(Key*)user; };
-  s32 index = Array::find_first(map, find_func, &key);
+  s32 index = find_index(map, key);
   if (index != -1)
   {
     result = &(Array::get(map, index)->value);
   }
 
   return result;
+}
+
+
+// NOTE:  Untested!
+template <typename Key, typename Value>
+Value&
+ensure(Map<Key, Value> const& map, Key key, Value value)
+{
+  Value* result = find(map, key);
+  if (result)
+  {
+    result->value = value;
+  }
+  else
+  {
+    result = Array::add(map, {key, value});
+  }
+  assert(value != NULL);
+  return *value;
+}
+
+
+template <typename Key, typename Value>
+bool
+erase(Map<Key, Value>& map, Key key)
+{
+  using PairT = Pair<Key, Value>;
+
+  Array::FindFirstFunc<PairT> find_func = [](PairT const& pair, void* user){ return pair.key == *(Key*)user; };
+  s32 index = Array::find_first(map, find_func, &key);
+  if (index != -1)
+  {
+    Array::remove(map, index);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 } // namespace Map
